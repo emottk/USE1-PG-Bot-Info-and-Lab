926f1d8b76b46ce184ba24ee1cb17262a83f10fb
emottk <emily.kowal@postlight.com>
2016-10-03 15:21:27 -0400

    add README and lab

--- lab.rb
+++ lab.rb
@@ -1,4 +1,12 @@
-puts "What is your favorite food?"
-	food = gets
+#Let's start with a Greeting
 
-print "Your favorite food is #{food}? So is mine!"
+puts "Hello, what is your name?"
+name = gets.chomp
+
+puts "Hi #{name}! I hope you're well."
+
+#Run this code before adding to it.
+#Take a look at how it works, and then add more questions.
+#Create a bot that can have a full and interactive conversation
+#with the user.
+#Some things you can ask for: Age, Birthday, Hometown, School, Eye color.
